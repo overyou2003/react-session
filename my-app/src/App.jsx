4b91ca388dddaa13559e32da9d6661a00ada95fb
Patiphat <youaremyhopes@gmail.com>
2025-09-13 21:34:12 +0700

    react

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,13 +1,24 @@
-
+import { useState } from "react"
 
 function App() {
-  const n = 1
-  let name = 'dream'
+  const [data,setData] = useState([
+    {id:1 , name:"Dreams" , gender:"Male"},
+    {id:2 , name:"Pleng" , gender:"Female"},
+    {id:3 , name:"Begle" , gender:"Female"}
+  ])
+
+  const [show,setShow] = useState(true)
+  console.table(data)
   return (
-    <div>
-       <h1>Dreams {name}</h1>
-       dasdad
-    </div>
+    <>
+      <h1>People : {data.length} pcs</h1>
+      <button onClick={()=> setShow(!show)}>{show ? "Hide" : "Show"}</button>
+      <ul>
+        {show && data.map((element)=>(
+          <li key={element.id}>{element.name} | {element.gender}</li>
+        ))}
+      </ul>
+    </>
   
   )
 }
